0ddf68e589505550d7f1a8e8c574726125217a3e
HienNgoc <ngochiensunflower@gmail.com>
2024-01-08 20:35:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import Home from './pages/home/Home';
+
+import Contact from './pages/contact/Contact';
+import NotFound from './components/notFound/NotFound';
+import Product_list from './pages/product_list/Product_list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/products" element={<Product_list />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
